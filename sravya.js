f717c3cff975cc333ac8318d996b3b9629dc74d4
Srinivas <122779862+roman-empire-ui@users.noreply.github.com>
2025-02-24 18:46:58 +0530

    first commit

---
+++ sravya.js
@@ -0,0 +1,33 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+console.log('menuIcon:', menuIcon);
+console.log('navbar:', navbar);
+console.log('sections:', sections);
+console.log('navLinks:', navLinks);
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            let activeLink = document.querySelector('header .navbar a[href*="' + id + '"]');
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
